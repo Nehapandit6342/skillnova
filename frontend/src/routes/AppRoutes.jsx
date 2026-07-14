da5c207e7e4ffb517fae9b4094c693bb630f5ac5
Neha Pandit <nehapandit9817@gmail.com>
2026-07-14 22:27:12 +0545

    solved conflict

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 
 import MainLayout from "@/layouts/MainLayout";
+
 import HomePage from "@/features/landing/pages/HomePage";
+import LoginPage from "@/features/auth/pages/LoginPage";
+import RegisterPage from "@/features/auth/pages/RegisterPage";
+import StudentLayout from "@/layouts/StudentLayout";
+import StudentDashboard from "@/features/student/pages/StudentDashboard";
+import StudentProfile from "@/features/student/pages/StudentProfile";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Public Website */}
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
+      </Route>
+
+      {/* Authentication */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      {/* Student */}
+      <Route path="/student" element={<StudentLayout />}>
+        <Route index element={<StudentDashboard />} />
+        <Route path="dashboard" element={<StudentDashboard />} />
+        <Route path="profile" element={<StudentProfile />} />
       </Route>
     </Routes>
   );
